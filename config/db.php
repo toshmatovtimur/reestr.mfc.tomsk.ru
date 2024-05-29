a69adaf7a1115d4e7222bc06764aecb33f1601d6
Тошматов Тимур <ttt10@tpu.ru>
2024-05-29 16:49:49 +0700

    123

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=localhost;dbname=yii2basic',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
